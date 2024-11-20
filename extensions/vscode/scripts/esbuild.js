2c792c44f362ecb2c3405ea43fcb029860a4cf3a
ZakariaBoukernafa <zakaria0697@gmail.com>
2024-11-20 13:44:23 +0100

    fix: update esbuild installation to a specific version and streamline build script

--- extensions/vscode/scripts/esbuild.js
+++ extensions/vscode/scripts/esbuild.js
@@ -11,24 +11,24 @@ const esbuildConfig = {
   platform: "node",
   sourcemap: flags.includes("--sourcemap"),
   loader: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     ".node": "file",
   },
+
+  // To allow import.meta.path for transformers.js
+  // https://github.com/evanw/esbuild/issues/1492#issuecomment-893144483
   inject: ["./scripts/importMetaUrl.js"],
   define: { "import.meta.url": "importMetaUrl" },
   supported: { "dynamic-import": false },
 };
 
 (async () => {
-  try {
-    if (flags.includes("--watch")) {
-      const ctx = await esbuild.context(esbuildConfig);
-      await ctx.watch();
-    } else {
-      await esbuild.build(esbuildConfig);
-    }
-    console.log("VS Code Extension esbuild complete");
-  } catch (error) {
-    console.error("Build failed:", error);
-    process.exit(1);
+  // Bundles the extension into one file
+  if (flags.includes("--watch")) {
+    const ctx = await esbuild.context(esbuildConfig);
+    await ctx.watch();
+  } else {
+    await esbuild.build(esbuildConfig);
   }
+  console.log("VS Code Extension esbuild complete"); // Used in task endpattern to signal completion
 })();
